b7c6806150fcd1b3186187f2907df866e0b56c1f
Patrick O <patrick.obiaso@ofsted.gov.uk>
2025-11-26 23:36:58 +0000

    eslint fix reference type added

---
+++ cypress/src/pageObjects/ItemInventory.ts
@@ -0,0 +1,6 @@
+class ItemInventory {
+  private readonly itemImg: string = '[data-test="img"]';
+  private readonly addToCart: string = '[data-test="add-to-cart"]';
+}
+
+export {ItemInventory};
--- cypress/support/sharedState.ts
+++ cypress/support/sharedState.ts
@@ -1,22 +1,33 @@
 type SharedData = Record<string, any>;
 
 class SharedState {
-  private static state: SharedData = {};
+  private static KEY = '__shared_state__';
+
+  private static getState(): SharedData {
+    return (Cypress.env(this.KEY) as SharedData) ?? {};
+  }
+
+  private static setState(state: SharedData): void {
+    Cypress.env(this.KEY, state);
+  }
 
   // Set a key-value pair
   static set(key: string, value: any): void {
-    this.state[key] = value;
+    const current = this.getState();
+    current[key] = value;
+    this.setState(current);
   }
 
   // Get a value by key
   static get<T>(key: string): T {
-    return this.state[key] as T;
+    const current = this.getState();
+    return current[key] as T;
   }
 
   // Clear all shared data
   static clear(): void {
     cy.log('Clearing shared state');
-    this.state = {};
+    this.setState({});
   }
 }
 
